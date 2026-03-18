5f1f12054db63bb6cfdb38b8910d1e18688abdb2
auntfunny <greenauntfunny@gmail.com>
2026-03-18 17:52:32 -0300

    Slight Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-
-import Header from './components/Header'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
-import Contact from "./pages/Contact"
-import About from "./pages/About"
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 import Individual from "./pages/Individual";
 
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/Contact' element={<Contact/>}></Route>
-        <Route path='/About' element={<About/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/About" element={<About />}></Route>
         <Route path="/meal/:id" element={<Individual />}></Route>
-       
       </Routes>
-    <Footer/>
+      <Footer />
     </>
   );
 }
