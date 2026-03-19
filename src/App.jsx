a23b7e473451744d9f15f27df7be6dc22211b430
Henry Poma <poma.hny@gmail.com>
2026-03-19 15:16:15 -0400

    meils

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-
-import { useState } from "react";
-import Header from "./components/Header";
-import Meils from "./pages/Meils";
-
-function App() {
-    return (
-        <>
-            <Meils />
-            <Header />
-        </>
-    );
-
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
@@ -19,22 +6,23 @@ import About from "./pages/About";
 import Individual from "./pages/Individual";
 import Footer from "./components/Footer";
 import WhatsappContact from "./components/WhatsappContact";
+import Meils from "./pages/Meils";
 
 function App() {
-  return (
-    <div  className='bg-accbg'>
-      <Header/>
-      <WhatsappContact/>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Contact" element={<Contact />}></Route>
-        <Route path="/About" element={<About />}></Route>
-        <Route path="/meal/:id" element={<Individual />}></Route>
-      </Routes>      
-      <Footer/>
-    </div>
-  )
-
+    return (
+        <div className="bg-accbg">
+            <Header />
+            <WhatsappContact />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/Contact" element={<Contact />}></Route>
+                <Route path="/About" element={<About />}></Route>
+                <Route path="/meal/:id" element={<Individual />}></Route>
+                <Route path="/meils" element={<Meils />}></Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
