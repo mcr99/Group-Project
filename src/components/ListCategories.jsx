2a72bb9310db15c47f7836aceec28488c7f3c190
marvin <mcanastuj1999@gmail.com>
2026-03-20 13:33:16 -0600

    adding error page, error window and adjusting header

--- src/components/ListCategories.jsx
+++ src/components/ListCategories.jsx
@@ -1,16 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
+function ListCategories() {
+    const [category, setCategory] = useState([]);
+    const { group, filter } = useParams();
 
-function ListCategories(props) {
-    
+    useEffect(() => {
+        async function categoryList() {
+            try {
+                const callCategory = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php");
+                setCategory(callCategory.data.categories);
+            } catch (error) {
+                console.log("Algo salio mal:", error);
+            }
+        }
+        categoryList();
+    }, []);
+
     return (
         <>
             <div className="w-10/12 mx-5 grid grid-cols-1 md:m-auto">
                 <p className="text-acc2 text-4xl mb-5 capitalize">categorias</p>
                 <div className=" w-12/12 grid grid-cols-2 gap-1 text-2xl md:grid md:grid-cols-3 lg:grid lg:grid-cols-5 ">
-                    {props.category.map((item) => (
-                        <p className="bg-acc2 rounded-xl px-5" key={item.idCategory}>
+                    {category.map((item) => (
+                        <Link
+                            to={`/meals/category/${item.strCategory}`}
+                            className={` rounded-xl px-5 ${filter.toLowerCase() === item.strCategory.toLowerCase() && group === "category" ? "bg-acc5 text-acc3" : "bg-acc2"}`}
+                            key={item.idCategory}>
                             {item.strCategory}
-                        </p>
+                        </Link>
                     ))}
                 </div>
             </div>
