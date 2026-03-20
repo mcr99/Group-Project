557d410ef0e0357251732ebec4ad6acf3577ddbf
auntfunny <greenauntfunny@gmail.com>
2026-03-20 13:18:51 -0300

    Links in Meals page

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
 function ListIngredients() {
     const [ingredients, setIngredients] = useState([]);
+    const { group, filter } = useParams();
 
     useEffect(() => {
         async function callIngredients() {
             try {
                 const IngredientesList = await axios.get("https://www.themealdb.com/api/json/v1/1/list.php?i=list");
                 setIngredients(IngredientesList.data.meals);
-                console.log(IngredientesList.data.meals);
             } catch (error) {
                 console.log("Algo salio mal:", error);
             }
@@ -22,9 +23,9 @@ function ListIngredients() {
                 <p className="text-acc2 text-2xl mb-5 capitalize">ingredients</p>
                 <div className=" w-70 flex flex-wrap gap-1 overflow-y-auto text-xl ">
                     {ingredients.slice(0, 15).map((item) => (
-                        <p className="bg-acc2 rounded-xl p-2" key={item.idIngredient}>
+                        <Link to={`/meals/ingredient/${item.strIngredient}`} className={`rounded-xl p-2 ${filter.toLowerCase() === item.strIngredient.toLowerCase() && group === "ingredient" ? "bg-acc5 text-acc3" : "bg-acc2"}`} key={item.idIngredient}>
                             {item.strIngredient}
-                        </p>
+                        </Link>
                     ))}
                 </div>
             </div>
--- src/components/WhatsappContact.jsx
+++ src/components/WhatsappContact.jsx
@@ -1,6 +1,6 @@
 function WhatsappContact(){
     return(
-        <div className="sticky top-[90%] p-2 z-200">
+        <div className="sticky top-[90%] p-2 z-200 w-11 place-self-end">
             <a href="https://wa.me/5021568465" target="_blank"> {/* random number added*/}
                 <img src="whatsapp.png" alt="whatsapp logo" className="w-11 h-11 absolute right-1"/>
             </a>
